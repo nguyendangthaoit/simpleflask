63382f8f50c85f951023d1c3ddad9cfc4008f42b
nguyendangthaoit <nguyendangthaoit@gmail.com>
2024-10-31 16:56:15 +0800

    init release

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Azure App Service Main!"
+    return "Hello, Azure App Service Release!"
 
 if __name__ == '__main__':
     app.run(debug=True)
